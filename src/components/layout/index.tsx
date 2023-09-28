a6ec0cf95cb3e514b6718f744caed54785ac766c
Alexandre <alexandre.o.souza12@gmail.com>
2023-09-28 14:11:16 -0300

    chore: change background color

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,14 +2,19 @@ import { Flex } from "@chakra-ui/react";
 import Footer from "./footer";
 import Header from "./header";
 import { WithChildren } from "@/types/commun";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }: WithChildren) {
+  const router = useRouter();
+  const color =
+    router.pathname === "/" ? "churras.primary_yellow" : "churras.white";
   return (
     <Flex
       height={"100vh"}
       width={"100%"}
       flexDirection={"column"}
       justifyContent={"space-between"}
+      background={color}
     >
       <Flex flexDirection={"column"} alignItems={"center"}>
         <Header />
